1300435685f469f4bba1da6b6fc86a8138518d81
DiazBryan <117124394+DiazBryan@users.noreply.github.com>
2024-03-14 19:31:21 -0500

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+	"./node_modules/tw-elements/js/**/*.js"],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("tw-elements/plugin.cjs")],
+	darkMode: "class"
+}
